02c6ea1eee22551cd9c41ac5f2f28272c985d34f
Mario Edgar <edgarm97@gmail.com>
2020-04-03 15:08:02 -0700

    data drive map, updated getData logic to stitch together the geoJson state data and the pandemic API data

--- webapp/build/getStaticData.js
+++ webapp/build/getStaticData.js
@@ -1,13 +1,49 @@
 const fs = require('fs')
 const path = require('path')
 const axios = require('axios')
+const stateAbbreviations = require('./stateAbbreviations')
+const baseStateGeoJson = require('./baseStateGeoJson')
+const API_URL = 'https://covidtracking.com/api/states'
+const OUTPUT_FILENAME = 'stateGeoJson.json'
 
-getAndSave('https://covidtracking.com/api/states', 'states.json')
+init()
 
-async function getAndSave(url, fileName) {
+async function init() {
+  try {
+    const data = await getData(API_URL)
+    const mappedStateData = mapStateData(data)
+    const stateGeoJson = mapGeoJsonStateData(mappedStateData)
+    saveData(stateGeoJson, OUTPUT_FILENAME)
+  } catch (err) {
+    console.error('There was an error generating the pandemic data. ', err) // eslint-disable-line
+  }
+}
+
+function mapGeoJsonStateData(mappedStateData) {
+  baseStateGeoJson.features = baseStateGeoJson.features.map((feature) => {
+    feature.properties = Object.assign(
+      feature.properties,
+      mappedStateData[feature.properties.name]
+    )
+    feature.properties.density = 0
+    return feature
+  })
+  return baseStateGeoJson
+}
+
+// convert state data to a map. key = full state name, value is the pandemic state data
+function mapStateData(stateData) {
+  return stateData.reduce((acc, item) => {
+    const fullStateName = stateAbbreviations[item.state]
+    acc[fullStateName] = item
+    return acc
+  }, {})
+}
+
+async function getData(url) {
   try {
     const { data } = await axios.get(url)
-    return saveData(data, fileName)
+    return data
   } catch (err) {
     return err
   }
--- webapp/store/states.js
+++ webapp/store/states.js
@@ -1,27 +1,81 @@
-// import states from '~/static/data/states.json'
 import geoJson from '~/static/data/stateGeoJson.json'
 
 const state = () => ({
-  geoJson
+  geoJson,
+  property: null,
+  selectedState: null,
+  highestState: null,
+  lowestState: null
 })
 
 const actions = {
-  // setDensityByProp({ commit }, property) {
-  //   // todo
-  //   // console.log('setDensityByProp: ', property)
-  //   // find the max value of all items first. then you can set everything from 0-100, two loops
-  // }
+  setDensityByProp({ commit, state }, property) {
+    // Highest State
+    const highestState = state.geoJson.features.reduce((acc, feature) => {
+      if (acc.properties[property] < feature.properties[property]) {
+        acc = feature
+      }
+      return acc
+    }, state.geoJson.features[0])
+    // Lowest State
+    const lowestState = state.geoJson.features.reduce((acc, feature) => {
+      if (acc.properties[property] > feature.properties[property]) {
+        acc = feature
+      }
+      return acc
+    }, state.geoJson.features[0])
+    // Get the ratio for calculating density between 0 - 1000
+    const ratio = 1000 / highestState.properties[property]
+    // Update density based on ratio for each state
+    geoJson.features.forEach((feature, index) => {
+      if (feature.properties[property]) {
+        const density = ratio * feature.properties[property]
+        const newFeature = { ...feature }
+        newFeature.properties = { ...feature.properties }
+        newFeature.properties.density = density
+        commit('SET_FEATURE', { index, newFeature })
+      }
+    })
+    // Mutate high low and selected property
+    commit('SET_PROPERTY', property)
+    commit('SET_HIGH_VALUE', highestState)
+    commit('SET_LOW_VALUE', lowestState)
+  }
 }
 
 const getters = {
   geoJson: (state) => {
     return state.geoJson
+  },
+  property: (state) => {
+    return state.property
+  },
+  highestState: (state) => {
+    return state.highestState
+  },
+  lowestState: (state) => {
+    return state.lowestState
+  },
+  selectedState: (state) => {
+    return state.selectedState
   }
 }
 
 const mutations = {
-  SET(state, geoJson) {
-    state.geoJson = geoJson
+  SET_FEATURE(state, { index, newFeature }) {
+    state.geoJson.features.splice(index, 1, newFeature)
+  },
+  SET_PROPERTY(state, property) {
+    state.property = property
+  },
+  SET_HIGH_VALUE(state, highestState) {
+    state.highestState = highestState
+  },
+  SET_LOW_VALUE(state, lowestState) {
+    state.lowestState = lowestState
+  },
+  SET_SELECTED_STATE(state, selectedState) {
+    state.selectedState = selectedState
   }
 }
 
