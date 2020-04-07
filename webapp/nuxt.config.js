cfc74dc40c1109b604a219417a32731b18b0f96f
Mario Edgar <edgarm97@gmail.com>
2020-04-07 15:48:23 -0700

    fix viewport

--- webapp/nuxt.config.js
+++ webapp/nuxt.config.js
@@ -25,6 +25,10 @@ export default {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width,initial-scale=1.0'
       }
     ],
     link: [
